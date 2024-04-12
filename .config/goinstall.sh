cc4d9db1d7515654ed17a2b35d04447f127669d9
wipdev-tech <wipdev.tech@gmail.com>
2024-04-12 14:17:58 +0200

    Add install script for Go tools

---
+++ .config/goinstall.sh
@@ -0,0 +1,6 @@
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+go install github.com/cosmtrek/air@latest
+go install github.com/a-h/templ/cmd/templ@latest
+go install github.com/pressly/goose/v3/cmd/goose@latest
+go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
